4fd8c02f497dfdad2019f650bcbd3f662885a03b
RicardoF4 <ricardo.fernandez2454@alumnos.udg.mx>
2021-05-29 17:35:20 -0500

    Responsive tweaks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const nav = document.querySelector(".nav");
 
+    document.querySelector("#btnNav").addEventListener("click", () => {
+        nav.classList.add("nav_open");
+    });
+
+    document.querySelector(".nav_overlay").addEventListener("click", () => {
+        nav.classList.remove("nav_open");
+    });
+});
