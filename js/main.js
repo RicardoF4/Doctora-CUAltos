22c48a4381bf96967b7af13ad4f39f543928d543
RicardoF4 <ricardo.fernandez2454@alumnos.udg.mx>
2021-06-03 16:28:02 -0500

    Responsividad completa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () =>{
     const nav = document.querySelector(".nav");
+    const pc = document.querySelector(".pc");
+    const mobil = document.querySelector(".mobil");
+    var mediaQueryList = window.matchMedia('(max-width: 720px)');
 
     document.querySelector("#btnNav").addEventListener("click", () => {
         nav.classList.add("nav_open");
@@ -8,4 +11,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     document.querySelector(".nav_overlay").addEventListener("click", () => {
         nav.classList.remove("nav_open");
     });
+
+    window.addEventListener('load', (event) => {
+        media(mediaQueryList);
+    });
+
+    function media(e){
+        if(e.matches){
+            pc.classList.add("hide");
+            mobil.classList.remove("hide");
+
+        }else{
+            pc.classList.remove("hide");
+            mobil.classList.add("hide");
+        }
+    }
+
+    mediaQueryList.addListener(media);
 });
